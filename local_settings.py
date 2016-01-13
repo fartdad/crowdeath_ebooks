d1859dee1da8ae97217a0104c369ec6a7f3230fe
fartdad <hatch_13@hotmail.com>
2016-01-13 07:37:33 -0700

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -4,9 +4,9 @@ Local Settings for a heroku_ebooks account. #fill in the name of the account you
 
 #configuration
 MY_CONSUMER_KEY = "puNs19ZVIDYBoSSMnb4dzqVhz"
-MY_CONSUMER_SECRET = ThFNOWH4zJ8rlsK4Zo3qFjHVbCEv8h8gDOxfFurPgtkkSLsk5Q
-MY_ACCESS_TOKEN_KEY = 3252887558-QzPkNnScuWqRLYargsAkDvfoYLoKVlwqS0k4c5g
-MY_ACCESS_TOKEN_SECRET = Docg6IAnBdLrfQqMYIUOyhskIk7PEiAqFQLah7o2CKem6
+MY_CONSUMER_SECRET = "ThFNOWH4zJ8rlsK4Zo3qFjHVbCEv8h8gDOxfFurPgtkkSLsk5Q"
+MY_ACCESS_TOKEN_KEY = "3252887558-QzPkNnScuWqRLYargsAkDvfoYLoKVlwqS0k4c5g"
+MY_ACCESS_TOKEN_SECRET = "Docg6IAnBdLrfQqMYIUOyhskIk7PEiAqFQLah7o2CKem6"
 
 SOURCE_ACCOUNTS = ["@crow_death"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 4 #How often do you want this to run? 1/8 times?
